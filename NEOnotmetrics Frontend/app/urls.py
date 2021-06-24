e9bdd88cf1aa8dea00cd13482b86ec692fea88ae
Patrick Timm <patze2000@freenet.de>
2021-06-24 18:19:38 +0200

    Dropdownbutton

--- NEOnotmetrics Frontend/app/urls.py
+++ NEOnotmetrics Frontend/app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('Calculation', views.RestResponse, name="RestResponse"),
+    path('Visualizations' ,views.Visualizations, name= "Visualizations.html"),
     path('head', views.header, name="header"),
     path('foot', views.footer, name="footer"),
 
