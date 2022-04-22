414d3097d3e6721d62e887231cb6c17a6c7b40fe
Achim Reiz <areiz@informatik.uni-rostock.de>
2022-04-22 15:10:55 +0200

    Updated Ports (Again)

--- frontend/lib/settings.dart
+++ frontend/lib/settings.dart
@@ -4,6 +4,6 @@ class Settings {
   /// Provides basic settings for the functioning of the app.
   /// Comparable to global variables.
   
-  final String apiUrl = (kDebugMode) ?  "http://localhost:8000": "http://fittony.gg01.local:8086";
+  final String apiUrl = (kDebugMode) ?  "http://localhost:8000": "http://api.neontometrics.informatik.uni-rostock.de";
   final String reasoningLimit = "0,3MB";
 }
